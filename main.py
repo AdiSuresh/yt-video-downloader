6a9a674176559ac0d8749d86809cae6c2736163d
ADITHYA SURESH <adithyasuresh201@gmail.com>
2021-08-29 18:52:52 +0530

    added replaceable_chars variable (has bugs!)

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from video_downloader import Downloader
 
 if __name__ == '__main__':
     downloader = Downloader()
-    downloader.download_from_url(url="https://youtu.be/eV_Lifvn6Sw")
+    downloader.download_from_url_file()
--- video_downloader.py
+++ video_downloader.py
@@ -6,27 +6,30 @@ class Downloader:
     def __init__(self):
         self.limit = 20
         self.download_path = "C:/Adithya/fy-project/downloads"
+        self.replaceable_chars = ['/', '|', ':']
 
     # noinspection PyMethodMayBeStatic
     def check_if_downloaded(self, title):
+        print(title)
         filenames = os.listdir(self.download_path)
         if len(filenames) == 0:
             return False
         filenames = [os.path.splitext(s)[0] for s in filenames]
-        title = title.replace("|", "_")
+        for char in self.replaceable_chars:
+            title = title.replace(char, "_")
         return title in filenames
 
     def download_from_url(self, url):
         video = pafy.new(url)
         if self.check_if_downloaded(video.title):
             print(f"\"{video.title}\" has already been downloaded!")
-            print("Please check the \"downloads\" folder")
-            print(f"{video.title}")
+            folder_name = os.path.basename(self.download_path)
+            print(f"Please check the \"{folder_name}\" folder\n")
         else:
             best = video.getbest()
             print("Downloading...")
             best.download(filepath="downloads/")
-            print(f"\"{video.title}\" downloaded!")
+            print(f"\"{video.title}\" downloaded!\n")
 
     def download_from_url_file(self):
         f = open("urls.txt", "r")
@@ -36,3 +39,4 @@ class Downloader:
         f.close()
         for url in list_of_urls:
             self.download_from_url(url=url)
+        print(os.listdir(self.download_path))
